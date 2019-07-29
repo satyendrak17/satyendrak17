43004af0a1b4a4011566e69dc3e867837af8f921
satyendrak <satyendra.kumar@cybertech.com>
2019-07-29 17:12:07 +0530

    dynamic angular forms controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'testapp';
+
+  orderForm: FormGroup;
+  items: FormArray;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.orderForm = this.formBuilder.group({
+      customerName: '',
+      email: '',
+      items: this.formBuilder.array([ this.createItem() ])
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  createItem(): FormGroup {
+    return this.formBuilder.group({
+      name: [null, Validators.required],
+      description: '',
+      price: ''
+    });
+  }
+
+  addItem(): void {
+    this.items = this.orderForm.get('items') as FormArray;
+    this.items.push(this.createItem());
+    // console.log('orderForm.get', this.orderForm.get('items').controls);
+
+    const arrayControl = this.orderForm.get('items') as FormArray;
+    console.log('arrayControl ', arrayControl.at(0).value);
+    // Print
+    // for (let i = 0; i < this.orderForm.controls.items.controls.length; i++) {
+    //  const tData = this.orderForm.controls.items[i].
+    //  console.log('Names are - ', );
+    // }
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { DemoMaterialModule } from '../material.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+  BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    DemoMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
