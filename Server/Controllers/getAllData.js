a9f2b6bde64d15d623924149fe6339de180972e4
satyendra kumar <satyendrak17@gmail.com>
2025-07-14 21:38:27 +0530

    stremlit app for billing software

--- Server/Controllers/getAllData.js
+++
@@ -1,8 +0,0 @@
-
-var Todo = require('../db/db').Todo;
-module.exports = function(req, res) {
-	Todo.find(function(error, resp) {
-	console.log('resp', resp);
-		res.send({response : resp});
-	});
-}
